fd0181f265b0f381c227632cff8c2c4746b4e506
Michael Dreesen <michaeldreesen90@gmail.com>
2020-11-21 00:08:23 -0700

    added removeUser function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -98,13 +98,12 @@ const userController = {
             .catch(err => res.json(err));
     },
     */
+
     removeFriend({ params }, res) {
-        User.findOneAndUpdate({ _id: params.userId }, { $pull: { friends: { friendId: params.friendId } } }, { new: true })
+        User.findOneAndUpdate({ _id: params.userId }, { $pull: { friends: params.friendId } }, { new: true, runValidators: true })
             .then(dbUserData => res.json(dbUserData))
             .catch(err => res.json(err))
     }
-
-
 
 };
 
